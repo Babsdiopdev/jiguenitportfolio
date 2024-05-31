0936390b9156e245adb5c27eb63ead035b687ef6
nogaye <babs78589@gmail.com>
2024-05-31 06:35:17 +0000

    modification du fihier mail.php

--- php/mail.php
+++ php/mail.php
@@ -1,20 +1,4 @@
-<!-- 
-<?php
-    if (isset($_POST['message'])) {
-        $entete  = 'MIME-Version: 1.0' . "\r\n";
-        $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $entete .= 'From: nogayediop973@gmail.com' . "\r\n";
-        $entete .= 'Reply-to: ' . $_POST['email'];
 
-        $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
-        <p><b>Email : </b>' . $_POST['email'] . '<br>
-        <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
-
-        $retour = mail('jiguenit2023@gmail.com', 'Envoi depuis page Contact', $message, $entete);
-        if($retour)
-            echo '<p>Votre message a bien été envoyé.</p>';
-    }
-    ?> -->
     <em> Merci 
     <?php
     echo strip_tags($_POST['name']);?>
